c76b8dc12e054dad60e7c977df61119d1b78263c
Juan david burgos oquendo <j.dburgos@hotmail.com>
2021-12-27 19:56:21 -0500

    test: test updated

--- src/components/helpers/Wrapper/Wrapper.test.js
+++ src/components/helpers/Wrapper/Wrapper.test.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 
 /** Components **/
 import Wrapper from './Wrapper';
+import { render, screen } from '@testing-library/react';
 
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Wrapper />, div);
-});
+describe('Wrapper component', () => {
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Wrapper><h1>test wrapper</h1></Wrapper>, div);
+  });
+
+  test('contains test text', () => {
+    render(<Wrapper><h1>test wrapper</h1></Wrapper>);
+    const wrapperElement = screen.getByText("test wrapper");
+
+    expect(wrapperElement).toBeInTheDocument();
+  })
+})
